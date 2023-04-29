da0d59d586229b135ede332c07d75ac136a379f2
HenriqueTogo <henrique.togo@gmail.com>
2023-04-29 18:42:35 -0300

    variáveis e operadores

---
+++ JavaScript/variables.js
@@ -0,0 +1,18 @@
+const PI = 3.14; // const não pode ter seu valor alterado
+let personName = 'Henrique'; // let -> escopo de bloco
+let age = 22; // Tipos de variáveis (number, string, boolean, object)
+let number1 = 10;
+let number2 = 20;
+
+console.log(`Welcome ${personName}, you are ${age} years old!`);
+
+//Adição
+console.log(number1 + number2);
+//Subtração
+console.log(number1 - number2);
+//Multiplicação
+console.log(number1 * number2);
+//Divisão
+console.log(number1 / number2);
+//Resto
+console.log(number2 % number1);
