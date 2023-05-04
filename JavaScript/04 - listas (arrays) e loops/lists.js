ef22188a5e9357b31e6d8264a4199cce002b1986
HenriqueTogo <henrique.togo@gmail.com>
2023-05-04 10:34:41 -0300

    listas e alguns métodos

---
+++ JavaScript/04 - listas (arrays) e loops/lists.js
@@ -0,0 +1,11 @@
+const alunos = ['João', 'Vitor', 'Marina'];
+const nome = 'Henrique';
+
+console.log(nome.charAt(2));
+
+console.log(alunos);
+console.log(alunos.pop());
+console.log(alunos);
+console.log(alunos.shift());
+alunos.push('Rafael');
+console.log(alunos);
