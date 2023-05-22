d2db348bee4745fbb1ecf83ff274039bc2a177aa
HenriqueTogo <henrique.togo@gmail.com>
2023-05-22 13:56:17 -0300

    component com build e style de uma noticia

--- WebComponents/src/Components/CardNews.js
+++ WebComponents/src/Components/CardNews.js
@@ -1,9 +1,88 @@
-class CardNews extends HTMLElement {
-    constructor () {
+class Cardnews extends HTMLElement {
+    constructor() {
         super();
+
         const shadow = this.attachShadow({mode: "open"});
-        shadow.innerHTML = "<h1>Hello Henrique</h1>";
+        shadow.appendChild(this.build());
+        shadow.appendChild(this.styles());
+    }
+
+    build() {
+        const componentRoot = document.createElement("div");
+        componentRoot.setAttribute("class", "card");
+
+        const cardLeft = document.createElement("div");
+        cardLeft.setAttribute("class", "card__left");
+
+        const autor = document.createElement("span");
+        autor.textContent = "By " + (this.getAttribute("autor") || "Anonymous");
+
+        const linkTitle = document.createElement("a");
+        linkTitle.textContent = this.getAttribute("title");
+        linkTitle.href = this.getAttribute("link-url");
+        
+        const newsContent = document.createElement("p");
+        newsContent.textContent = this.getAttribute("content");
+        
+        cardLeft.appendChild(autor);
+        cardLeft.appendChild(linkTitle);
+        cardLeft.appendChild(newsContent);
+
+        
+        const cardRight = document.createElement("div");
+        cardRight.setAttribute("class", "card__right");
+        const newsImage = document.createElement("img");
+
+        newsImage.src = this.getAttribute("photo");
+        newsImage.alt = "Darth Vader";
+        cardRight.appendChild(newsImage);
+        
+        componentRoot.appendChild(cardLeft);
+        componentRoot.appendChild(cardRight);
+        
+        return componentRoot;
+    }
+
+    styles() {
+        const style = document.createElement("style");
+
+        style.textContent = `
+            .card {
+                width: 80%;
+                display: flex;
+                flex-direction: row;
+                box-shadow: 4px 6px 24px 0px rgba(0,0,0,0.75);
+                -webkit-box-shadow: 4px 6px 24px 0px rgba(0,0,0,0.75);
+                -moz-box-shadow: 4px 6px 24px 0px rgba(0, 0, 0, 0.75);
+                justify-content: space-between;
+            }
+
+
+            .card__left {
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                padding-left: 10px;
+            }
+
+            .card__left > span {
+                font-weight: 400;
+            }
+
+            .card__left > a {
+                margin-top: 15px;
+                font-size: 25px;
+                color: black;
+                text-decoration: none;
+                font-weight: bold;
+            }
+
+            .card__left > p {
+                color: rgb(70, 70, 70);
+            }
+            `;
+        return style;
     }
 }
 
-customElements.define('card-news', CardNews)
+customElements.define('card-news', Cardnews);
