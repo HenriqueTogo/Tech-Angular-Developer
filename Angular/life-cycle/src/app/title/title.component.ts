df024c764dc2981f8360e6c9b73635110b86e625
HenriqueTogo <henrique.togo@gmail.com>
2023-05-24 11:50:24 -0300

    life cycle angular

--- Angular/life-cycle/src/app/title/title.component.ts
+++ Angular/life-cycle/src/app/title/title.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
   styleUrls: ['./title.component.css']
 })
-export class TitleComponent {
+export class TitleComponent implements OnInit, OnChanges{
 
+  @Input() nome: string = '';
+  constructor() {
+    console.log(`Constructor ${this.nome}`);
+    
+  }
+  ngOnChanges(): void {
+    console.log(`OnChange ${this.nome}`);
+    
+  }
+  ngOnInit(): void {
+    this.nome = this.nome + 'X';
+    console.log(`OnInit ${this.nome}`)
+  }
 }
