43e1a5be1d0fe2df08cf9514eff1d695cceff3a4
HenriqueTogo <henrique.togo@gmail.com>
2023-05-25 18:16:24 -0300

    routing content

--- Angular/projects/blog/src/app/data/dataFake.ts
+++ Angular/projects/blog/src/app/data/dataFake.ts
@@ -1,14 +1,26 @@
 export const dataFake = [
     {
-        "id":1,
-        "photo": 'aaa',
-        "title": 'saiu novo filme',
-        'desciption': 'ola'
+        'id':'1',
+        'title': 'Novo filme do Batman',
+        'description': 'Novo filme do Batman é anunciado, confira',
+        'photoCover':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDBcI4sswzSbXJEQElBf-JScsq0m9E4YT4zGIbOr4MVHnrgv9pF8eZnIArHK4Ph-g33FA&usqp=CAU'
     },
     {
-        "id":2,
-        "photo": 'aaa',
-        "title": 'saiu novo filme',
-        'desciption': 'ola'
+        'id':'2',
+        'title': 'Série anunciada no Disney+',
+        'description': 'Acnunciada a nova série da Disney',
+        'photoCover':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgwHlx4GkwjAYR1pbOKZ3AQ-cJqo0RStsLfw&usqp=CAU'
+    },
+    {
+        'id':'3',
+        'title': 'Série anunciada no Star+',
+        'description': 'Acnunciada a nova série na Star+',
+        'photoCover':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSngmn7k5VBMOPn2nhUycojc_KUsRf0UGSxFw&usqp=CAU'
+    },
+    {
+        'id':'4',
+        'title': 'Série anunciada no Discovery+',
+        'description': 'Acnunciada a nova série na Discovery+',
+        'photoCover':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTv2fz3sGmD_uo2eMET8MTY0JWlm_BjkRfUGg&usqp=CAU'
     }
 ]
--- Angular/projects/blog/src/app/pages/content/content.component.ts
+++ Angular/projects/blog/src/app/pages/content/content.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { dataFake } from '../../data/dataFake';
+
 
 @Component({
   selector: 'app-content',
@@ -7,16 +9,28 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
-  photoCover:string = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTX4hPsxOAesfq6RAjycmTMRKUZFS_HlaBwtw&usqp=CAU';
-  contentTitle:string = 'Meu Artigo';
-  contentDescription:string = 'Filme do Homem-Aranha';
+  photoCover:string = '';
+  contentTitle:string = '';
+  contentDescription:string = '';
+  private id: string | null = '0';
+  
   constructor(
     private route:ActivatedRoute
   ){}
 
   ngOnInit(): void{
     this.route.paramMap.subscribe( value =>
-      console.log(value.get("id"))
+      this.id = value.get("id")
     )
+    this.setValuesToComponent(this.id);
   }
+
+  setValuesToComponent(id:string | null){
+    const result = dataFake.filter(article => article.id == id)[0]
+    this.contentTitle =  result.title;
+    this.contentDescription = result.description;
+    this.photoCover = result.photoCover;
+  }
+
+
 }
