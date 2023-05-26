f791cc39aa4b9102f97bad18f3889a284a5a3cb9
HenriqueTogo <henrique.togo@gmail.com>
2023-05-26 15:16:24 -0300

    services class

--- Angular/services/src/app/components/card/card.component.ts
+++ Angular/services/src/app/components/card/card.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PokemonData } from 'src/app/models/pokemonData';
+import { PokemonService } from 'src/app/services/pokemon.service';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
-export class CardComponent {
+export class CardComponent implements OnInit{
+  pokemon: PokemonData = {
+    id: 0,
+    name: '',
+    sprites: {
+      front_default: ''
+    },
+    types: []
+
+  };
+  
+  constructor(
+    private service:PokemonService
+  ) {
+
+  }
+
+  ngOnInit(): void {
+    this.getPokemon('charizard')
+  }
+
+  getPokemon(searchName: string){
+    this.service.getPokemon(searchName).subscribe(
+      {
+        next: (res) => {
+          this.pokemon = {
+            id: res.id,
+            name: res.name,
+            sprites: res.sprites,
+            types: res.types
+          }
+          console.log(res)
+          console.log(this.pokemon);
+        },
+        error: (res) => console.log('not found')
+      }
+    )    
+  }
 
 }
