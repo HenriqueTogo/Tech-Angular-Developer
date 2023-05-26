67eeabf39a256be15de9f09b118aa3f0f94a7457
HenriqueTogo <henrique.togo@gmail.com>
2023-05-25 22:41:35 -0300

    updated the tv series data

--- Angular/projects/blog/src/app/data/dataFake.ts
+++ Angular/projects/blog/src/app/data/dataFake.ts
@@ -1,26 +1,26 @@
 export const dataFake = [
     {
         'id':'1',
-        'title': 'Novo filme do Batman',
-        'description': 'Novo filme do Batman é anunciado, confira',
-        'photoCover':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDBcI4sswzSbXJEQElBf-JScsq0m9E4YT4zGIbOr4MVHnrgv9pF8eZnIArHK4Ph-g33FA&usqp=CAU'
+        'title': 'Peaky Blinders',
+        'description': 'Uma notória gangue da Inglaterra de 1919 é liderada pelo cruel Tommy Shelby, um criminoso disposto a subir na vida a qualquer preço.',
+        'photoCover':'https://musicnonstop.uol.com.br/wp-content/uploads/2022/02/60-609425_peaky-blinders-wallpapers-group-peaky-blinders-wallpaper-4k.jpg'
     },
     {
         'id':'2',
-        'title': 'Série anunciada no Disney+',
-        'description': 'Acnunciada a nova série da Disney',
-        'photoCover':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgwHlx4GkwjAYR1pbOKZ3AQ-cJqo0RStsLfw&usqp=CAU'
+        'title': 'Breaking Bad',
+        'description': 'Ao saber que tem câncer, um professor passa a fabricar metanfetamina pelo futuro da família, mudando o destino de todos.',
+        'photoCover':'https://addons-media.operacdn.com/media/CACHE/images/themes/85/172285/1.0-rev1/images/50b41d81-8185-4a3a-9227-7553dfb4d749/11abb37935c9f6f6929ae4491255c7a3.jpg'
     },
     {
         'id':'3',
-        'title': 'Série anunciada no Star+',
-        'description': 'Acnunciada a nova série na Star+',
-        'photoCover':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSngmn7k5VBMOPn2nhUycojc_KUsRf0UGSxFw&usqp=CAU'
+        'title': 'Game of Thrones',
+        'description': 'Game of Thrones conta a história de um lugar onde uma força destruiu o equilíbrio das estações, há muito tempo. Em uma terra onde os verões podem durar vários anos e o inverno toda uma vida, as reivindicações e as forças sobrenaturais correm as portas do Reino dos Sete Reinos. A irmandade da Patrulha da Noite busca proteger o reino de cada criatura que pode vir de lá da Muralha, mas já não tem os recursos necessários para garantir a segurança de todos. Depois de um verão de dez anos, um inverno rigoroso promete chegar com um futuro mais sombrio. Enquanto isso, conspirações e rivalidades correm no jogo político pela disputa do Trono de Ferro, o símbolo do poder absoluto.',
+        'photoCover':'https://c4.wallpaperflare.com/wallpaper/578/50/538/tv-show-game-of-thrones-wallpaper-preview.jpg'
     },
     {
         'id':'4',
-        'title': 'Série anunciada no Discovery+',
-        'description': 'Acnunciada a nova série na Discovery+',
-        'photoCover':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTv2fz3sGmD_uo2eMET8MTY0JWlm_BjkRfUGg&usqp=CAU'
+        'title': 'The Last of Us',
+        'description': 'The Last of Us é uma série distópica da HBO baseada na franquia de jogos de videogame de mesmo nome criada por Neil Druckmann. O drama narra um futuro pandêmico que foi devastador para humanidade, deixando os seres humanos à beira da extinção. O vírus transforma pessoas em canibais e se espalha rapidamente com uma simples mordida. Algumas décadas depois, os poucos sobreviventes que restaram vivem viajando ou em quarentenas protegidas por oficiais do governo. Este é o cenário em que acompanhamos Joel (Pedro Pascal), um sobrevivente durão que é contratado para levar a jovem Ellie (Bella Ramsey) para fora da zona de quarentena opressiva onde vive. Os dois devem encontrar um grupo de rebeldes paramilitares que se rebelou contra as autoridades. Porém, eles descobrem no caminho que Ellie está possivelmente infectada com o vírus, apesar da jovem não apresentar os sintomas recorrentes da doença. Por conta disso, eles acreditam que a imunidade de Ellie pode ser a chave para encontrar a cura do vírus e potencialmente salvar a humanidade.',
+        'photoCover':'https://t2.tudocdn.net/653854'
     }
 ]
